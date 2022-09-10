ca7979302a194e8a105504f4f5c41cb1eb52905a
Ujjal <khadkauzal@gmail.com>
2022-09-10 12:16:34 +0545

    5-Hidden-Layer-Activation-Functions

--- basic_neural_network.py
+++ basic_neural_network.py
@@ -1,11 +1,13 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
-X = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+X, y = spiral_data(100, 3)
 
 
-class LayerDense:
+class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         # weights are set with the number of rows as n_inputs such that we do not have to transpose the input matrix during the dot product
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -15,11 +17,23 @@ class LayerDense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-layer1 = LayerDense(4, 5)
-# the output of the first layer should match the input of the second layer
-layer2 = LayerDense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(2, 5)  # 0 - inputs, 1 - neurons
+
+activation1 = Activation_ReLU()
+
 
 layer1.forward(X)
-# print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+
+print(layer1.output)
+print(activation1.output)
+
+# the output of the first layer should match the input of the second layer
+# layer2 = Layer_Dense(5, 2)
+# layer2.forward(layer1.output)
+# print(layer2.output)
